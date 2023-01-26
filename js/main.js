6ecacbd53dfefd4f361fc9c4e589e407c8f3de8d
Ilya <113237750+Ilya-Stoyanov@users.noreply.github.com>
2023-01-26 11:27:00 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,44 +1,40 @@
 $(document).ready(function () {
-    // starts
     $(".customer-starts").starRating({
         totalStars:5,
         strokeColor: '#F1B90B',
         ratedColor: '#F1B90B',
-        strokeWidth: 10,
-        starSize: 22,
-        readOnly: true
+        starSize: 23,
+        readOnly: true,
+        strokeWidth: 20,
       });
 
-    //  slick slider
+    //   slider
     $(".customers-slider").slick({
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay: false,
-        autoplaySpeed: 2000,
-
-        prevArrow: '<button class="slider-prev slider-btn btn active"></button>',
-        nextArrow: '<button class="slider-next slider-btn btn"></button>',
+        prevArrow: '<button class="btn slider-btn slider-prev"></button>',
+        nextArrow: '<button class="btn slider-btn slider-next active"></button>',
         responsive: [
             {
                 breakpoint: 981,
                 settings: {
-                  arrows: false,
-                  dots: true
+                    arrows: false,
+                    dots: true
                 }
-            }
+              },
         ]
     });
 
     $('.slider-btn').click(function(){
-        $('.slider-btn').removeClass('active');
-        $(this).addClass('active');
-
-    })
+        $('.slider-btn').removeClass("active");
+        $(this).addClass("active")
+    });
 
     $(".burger").click(function(){
         $(".burger").toggleClass("active")
-        $(".nav").toggleClass("open")
-    })
+        $(".nav").toggleClass("active")
+        $("body").toggleClass("hidden")
 
+    })
 });
